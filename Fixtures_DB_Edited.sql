09760d7db973ab406fb860ac1c3e315c21f1c2d5
HurryNair <58472023+HurryNair@users.noreply.github.com>
2019-12-04 17:22:14 +0530

    Add files via upload

---
+++ Fixtures_DB_Edited.sql
@@ -0,0 +1,487 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fixtures
+-- ------------------------------------------------------
+-- Server version	5.7.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `collection`
+--
+
+DROP TABLE IF EXISTS `collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `collection` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `manu` FOREIGN KEY (`ID`) REFERENCES `manufacturer` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `collection`
+--
+
+LOCK TABLES `collection` WRITE;
+/*!40000 ALTER TABLE `collection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `collection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `design_style`
+--
+
+DROP TABLE IF EXISTS `design_style`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `design_style` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `design_style`
+--
+
+LOCK TABLES `design_style` WRITE;
+/*!40000 ALTER TABLE `design_style` DISABLE KEYS */;
+/*!40000 ALTER TABLE `design_style` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dimension`
+--
+
+DROP TABLE IF EXISTS `dimension`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dimension` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Width` varchar(30) NOT NULL,
+  `Height` varchar(30) NOT NULL,
+  `DEPTH` varchar(30) NOT NULL,
+  `CUT_OUT` varchar(50) DEFAULT NULL,
+  `Std_len` varchar(30) DEFAULT NULL,
+  `Mounting_Pipe_Diameter` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dimension`
+--
+
+LOCK TABLES `dimension` WRITE;
+/*!40000 ALTER TABLE `dimension` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dimension` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fixture_type`
+--
+
+DROP TABLE IF EXISTS `fixture_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fixture_type` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fixture_type`
+--
+
+LOCK TABLES `fixture_type` WRITE;
+/*!40000 ALTER TABLE `fixture_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fixture_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `holder_type`
+--
+
+DROP TABLE IF EXISTS `holder_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `holder_type` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `holder_type`
+--
+
+LOCK TABLES `holder_type` WRITE;
+/*!40000 ALTER TABLE `holder_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `holder_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `image` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Full_Image` varchar(100) NOT NULL,
+  `Thumbnail` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `image`
+--
+
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `location` (
+  `Name` varchar(30) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Phone Number` varchar(45) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `vendor_id` FOREIGN KEY (`ID`) REFERENCES `vendor` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturer`
+--
+
+DROP TABLE IF EXISTS `manufacturer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manufacturer` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `URL` varchar(100) NOT NULL,
+  `POC` varchar(14) NOT NULL,
+  `Image` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `Image` FOREIGN KEY (`ID`) REFERENCES `image` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturer`
+--
+
+LOCK TABLES `manufacturer` WRITE;
+/*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `physical_attrib`
+--
+
+DROP TABLE IF EXISTS `physical_attrib`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `physical_attrib` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Material` varchar(50) NOT NULL,
+  `Finish` varchar(50) NOT NULL,
+  `Body_Color` varchar(50) NOT NULL,
+  `Shape` varchar(50) NOT NULL,
+  `Glass_size` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `dimension` FOREIGN KEY (`ID`) REFERENCES `dimension` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `physical_attrib`
+--
+
+LOCK TABLES `physical_attrib` WRITE;
+/*!40000 ALTER TABLE `physical_attrib` DISABLE KEYS */;
+/*!40000 ALTER TABLE `physical_attrib` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pricebook`
+--
+
+DROP TABLE IF EXISTS `pricebook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pricebook` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `Eff_Strt_date` varchar(50) NOT NULL,
+  `Eff_end_date` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pricebook`
+--
+
+LOCK TABLES `pricebook` WRITE;
+/*!40000 ALTER TABLE `pricebook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pricebook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(25) DEFAULT NULL,
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `designStyle` FOREIGN KEY (`ID`) REFERENCES `design_style` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fixtureType` FOREIGN KEY (`ID`) REFERENCES `fixture_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `manufacturer` FOREIGN KEY (`ID`) REFERENCES `manufacturer` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `physicalAttribute` FOREIGN KEY (`ID`) REFERENCES `physical_attrib` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `technicalAttribute` FOREIGN KEY (`ID`) REFERENCES `technical_attrib` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `warranty` FOREIGN KEY (`ID`) REFERENCES `warranty` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_collection`
+--
+
+DROP TABLE IF EXISTS `product_collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_collection` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `collection` FOREIGN KEY (`ID`) REFERENCES `collection` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `productID` FOREIGN KEY (`ID`) REFERENCES `product` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_collection`
+--
+
+LOCK TABLES `product_collection` WRITE;
+/*!40000 ALTER TABLE `product_collection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_collection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_images`
+--
+
+DROP TABLE IF EXISTS `product_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_images` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `product` FOREIGN KEY (`ID`) REFERENCES `product` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `productImage` FOREIGN KEY (`ID`) REFERENCES `image` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_images`
+--
+
+LOCK TABLES `product_images` WRITE;
+/*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_price`
+--
+
+DROP TABLE IF EXISTS `product_price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_price` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `MRP` varchar(50) NOT NULL,
+  `Currency` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `pricebook` FOREIGN KEY (`ID`) REFERENCES `pricebook` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_price`
+--
+
+LOCK TABLES `product_price` WRITE;
+/*!40000 ALTER TABLE `product_price` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_price` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_stock`
+--
+
+DROP TABLE IF EXISTS `product_stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_stock` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `location_id` FOREIGN KEY (`ID`) REFERENCES `location` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `product_id` FOREIGN KEY (`ID`) REFERENCES `product` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_stock`
+--
+
+LOCK TABLES `product_stock` WRITE;
+/*!40000 ALTER TABLE `product_stock` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `technical_attrib`
+--
+
+DROP TABLE IF EXISTS `technical_attrib`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `technical_attrib` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Wattage` int(11) NOT NULL,
+  `LED_Source` varchar(50) NOT NULL,
+  `Beam_Angle` int(11) DEFAULT NULL,
+  `Color_Temperature` varchar(10) NOT NULL,
+  `Lumen_Start` varchar(10) NOT NULL,
+  `Lumen_End` varchar(10) NOT NULL,
+  `IP_Rating` varchar(10) NOT NULL,
+  `Input_Voltage` varchar(10) DEFAULT NULL,
+  `Output_Voltage` varchar(10) DEFAULT NULL,
+  `LED_mtr` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `holderType` FOREIGN KEY (`ID`) REFERENCES `holder_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `technical_attrib`
+--
+
+LOCK TABLES `technical_attrib` WRITE;
+/*!40000 ALTER TABLE `technical_attrib` DISABLE KEYS */;
+/*!40000 ALTER TABLE `technical_attrib` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vendor`
+--
+
+DROP TABLE IF EXISTS `vendor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vendor` (
+  `Name` varchar(30) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `pri_location` FOREIGN KEY (`ID`) REFERENCES `location` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vendor`
+--
+
+LOCK TABLES `vendor` WRITE;
+/*!40000 ALTER TABLE `vendor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vendor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `warranty`
+--
+
+DROP TABLE IF EXISTS `warranty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `warranty` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Coverage` varchar(50) NOT NULL,
+  `Terms_conditions` varchar(10000) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warranty`
+--
+
+LOCK TABLES `warranty` WRITE;
+/*!40000 ALTER TABLE `warranty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `warranty` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-04 14:09:34
