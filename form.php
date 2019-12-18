83dd12d4121b4f146f3229372016316ffe31f717
Harishankar S <58472023+HurryNair@users.noreply.github.com>
2019-12-18 14:17:25 +0530

    Add files via upload
    SQL script helps create MySQL Relational DB and insert data from into relevant columns from CSV files.

---
+++ form.php
@@ -0,0 +1,105 @@
+<form action="script.php" method="post"/>
+
+<p>DIMENSION</p>
+<p>Width(mm): <input type="text" name="input" /></p>
+<p>Height(mm): <input type="text" name="input1" /></p>
+<p>Depth(mm): <input type="text" name="input2" /></p>
+<p>Dia(mm): <input type="text" name="input44" /></p>
+<p>Cutout(mm): <input type="text" name="input3" /></p>
+<p>Standard Length(mm): <input type="text" name="input4" /></p>
+<p>Mounting-Pipe diameter(mm): <input type="text" name="input5" /></p>
+
+<p>HOLDER TYPE</p>
+<p>Name: <input type="text" name="input6" /></p>
+
+<p>WARRANTY</p>
+<p>Coverage(months): <input type="text" name="input7" /></p>
+<p>Terms & Conditions: <input type="text" name="input8" /></p>
+
+<p>PRICEBOOK</p>
+<p>Name: <input type="text" name="input9" /></p>
+<p>Start date: <input type="text" name="input10" /></p>
+<p>End date: <input type="text" name="input11" /></p>
+
+<p>IMAGES</p>
+<p>Full Image path: <input type="text" name="input12" /></p>
+<p>Thumbnail path: <input type="text" name="input13" /></p>
+
+<p>DESIGN_STYLE</p>
+<p>Name: <input type="text" name="input14" /></p>
+
+<p>VENDOR</p>
+<p>Name: <input type="text" name="input15" /></p>
+
+<p>PHYSICAL ATTRIBUTES</p>
+<p>Material: <input type="text" name="input16" /></p>
+<p>Finish: <input type="text" name="input17" /></p>
+<p>Body Color: <input type="text" name="input18" /></p>
+<p>Shape: <input type="text" name="input19" /></p>
+<p>Glass size: <input type="text" name="input20" /></p>
+
+<p>TECHNICAL ATTRIBUTES</p>
+<p>Wattage(W): <input type="text" name="input21" /></p>
+<p>LED Source: <input type="text" name="input22" /></p>
+<p>Beam Angle(dg): <input type="text" name="input23" /></p>
+<p>Color temperature(K): <input type="text" name="input24" /></p>
+<p>Lumen Start: <input type="text" name="input25" /></p>
+<p>Lumen End: <input type="text" name="input26" /></p>
+<p>IP Rating: <input type="int" name="input27" /></p>
+<p>Input Voltage(V): <input type="text" name="input28" /></p>
+<p>Output Voltage(V): <input type="text" name="input29" /></p>
+<p>LED/mtr: <input type="text" name="input30" /></p>
+
+<p>Manufacturer</p>
+<p>Name: <input type="text" name="input31" /></p>
+<p>URL: <input type="text" name="input32" /></p>
+<p>E-mail: <input type="text" name="input33" /></p>
+<p>Phone: <input type="text" name="input46" /></p>
+
+<p>LOCATION</p>
+<p>Name: <input type="text" name="input34" /></p>
+<p>Phone Number: <input type="text" name="input35" /></p>
+<p>Address: <input type="text" name="input36" /></p>
+<p>E-mail: <input type="text" name="input45" /></p>
+
+<p>PRODUCT</p>
+<p>product_code: <input type="text" name="input37" /></p>
+<p>Name: <input type="text" name="input38" /></p>
+
+<p>COLLECTION</p>
+<p>Name: <input type="text" name="input39" /></p>
+
+<p>PRODUCT PRICE</p>
+<p>MRP: <input type="text" name="input40" /></p>
+<p>Currency: <input type="text" name="input41" /></p>
+
+<p>PRODUCT STOCK</p>
+<p>Quantity(units): <input type="text" name="input42" /></p>
+
+<p>FIXTURE TYPE</p>
+<p>Name: <input type="text" name="input43" /></p>
+
+<input type="submit" value="Submit"/>
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
